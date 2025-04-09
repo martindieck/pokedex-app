0518232cc307142ccd0042b6920872650ee7c30d
martindieck-syscap <krutterlax@gmail.com>
2025-04-09 15:33:45 -0600

    Added Mailing support for account recovery and confirmation

--- Gemfile
+++ Gemfile
@@ -46,6 +46,9 @@ gem "httparty"
 # Add postgtres support
 gem "pg"
 
+# Added dotenv support
+gem "dotenv-rails"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,18 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "https://pokeclicker-da8e8cf35775.herokuapp.com/" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            ENV["EMAIL_USERNAME"],
+    password:             ENV["EMAIL_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
+
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
