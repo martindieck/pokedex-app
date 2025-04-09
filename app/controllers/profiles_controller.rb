f35e0b30e81d19559a225d596e57d746f5911bf9
martindieck-syscap <krutterlax@gmail.com>
2025-04-09 11:47:59 -0600

    Sorted pokemon in profiles page and added pokedex numbers

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,11 +2,11 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @caught_pokemons = current_user.pokemons.joins(:user_pokemons).where(user_pokemons: { shiny: false }).distinct
+    @caught_pokemons = current_user.pokemons.joins(:user_pokemons).where(user_pokemons: { shiny: false }).distinct.sort_by(&:pokedex_number)
     @caught_count = @caught_pokemons.count
     @total_pokemon = 151  # Total number of original Pokémon
 
-    @caught_shinies = current_user.pokemons.joins(:user_pokemons).where(user_pokemons: { shiny: true }).distinct
+    @caught_shinies = current_user.pokemons.joins(:user_pokemons).where(user_pokemons: { shiny: true }).distinct.sort_by(&:pokedex_number)
     @shiny_count = @caught_shinies.count
   end
 end
