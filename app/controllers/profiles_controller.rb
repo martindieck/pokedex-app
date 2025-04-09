16c37bd8ee47b0485af559cf57f8f621fd3552cc
martindieck-syscap <krutterlax@gmail.com>
2025-04-09 07:10:46 -0600

    Added caught pokemon counter to profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,7 @@ class ProfilesController < ApplicationController
 
   def show
     @caught_pokemons = current_user.pokemons
+    @caught_count = @caught_pokemons.count
+    @total_pokemon = 151  # Total number of original Pokémon
   end
 end
