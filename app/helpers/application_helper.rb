a95967ee5d9a3a17b9d444269fe3423e7838fa2b
martindieck-syscap <krutterlax@gmail.com>
2025-04-21 20:22:36 -0600

    Added number formatting using helper functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def format_number(number)
+    case number
+    when 1_000..999_999 then "#{(number / 1_000.0).round(3)}k"
+    when 1_000_000..999_999_999 then "#{(number / 1_000_000.0).round(3)}m"
+    when 1_000_000_000..Float::INFINITY then "#{(number / 1_000_000_000.0).round(3)}b"
+    else
+      "#{number.round(3)}"
+    end
+  end
 end
