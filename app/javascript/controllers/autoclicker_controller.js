a116326632044417f12a304d6ba7ecb97883f30f
martindieck-syscap <krutterlax@gmail.com>
2025-04-21 20:39:04 -0600

    Added support for decimal rates while keeping integers intact

--- app/javascript/controllers/autoclicker_controller.js
+++ app/javascript/controllers/autoclicker_controller.js
@@ -7,13 +7,17 @@ export default class extends Controller {
     userId: Number
   }
 
+  constructor() {
+    super()
+    this.runningDecimalSum = 0 // To store the sum of decimal parts
+  }
+
   connect() {
     this.startAutoclicking()
   }
 
   disconnect() {
     clearInterval(this.syncInterval)
-
   }
 
   startAutoclicking() {
@@ -24,13 +28,28 @@ export default class extends Controller {
 
   syncCatchCountWithServer() {
     const localPokemon = this.rateValue
+    const integerPart = Math.floor(localPokemon)
+    const decimalPart = localPokemon - integerPart
+
+    // Accumulate the decimal part
+    this.runningDecimalSum += decimalPart
+
+    // If the running decimal sum reaches or exceeds 1, add whole integers to the integer part
+    let wholeIntegers = Math.floor(this.runningDecimalSum)
+    if (wholeIntegers > 0) {
+      this.runningDecimalSum -= wholeIntegers // Remove the whole integers from the running sum
+    }
+
+    // Send the integer part plus any whole integers from the decimal sum
+    const totalCatchCount = integerPart + wholeIntegers
+
     fetch(`/encounters/increment_catch_count`, {
       method: "POST",
       headers: {
         "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ catch_count: localPokemon })
+      body: JSON.stringify({ catch_count: totalCatchCount })
     })
   }
 }
--- app/javascript/controllers/money_controller.js
+++ app/javascript/controllers/money_controller.js
@@ -7,6 +7,11 @@ export default class extends Controller {
     catchCount: Number
   }
 
+  constructor() {
+    super()
+    this.runningDecimalSum = 0 // To store the sum of decimal parts
+  }
+
   connect() {
     this.startEarning()
   }
@@ -21,16 +26,30 @@ export default class extends Controller {
     }, 1000)
   }
 
-
   syncBalanceWithServer() {
     const localEarnings = this.rateValue * this.catchCountValue
+    const integerPart = Math.floor(localEarnings)
+    const decimalPart = localEarnings - integerPart
+
+    // Accumulate the decimal part
+    this.runningDecimalSum += decimalPart
+
+    // If the running decimal sum reaches or exceeds 1, add whole integers to the earnings
+    let wholeIntegers = Math.floor(this.runningDecimalSum)
+    if (wholeIntegers > 0) {
+      this.runningDecimalSum -= wholeIntegers // Remove the whole integers from the running sum
+    }
+
+    // Add the whole integers from the decimal sum to the integer part of earnings
+    const totalEarnings = integerPart + wholeIntegers
+
     fetch(`/encounters/increment_balance`, {
       method: "POST",
       headers: {
         "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ earnings: localEarnings })
+      body: JSON.stringify({ earnings: totalEarnings })
     }).then(() => {
       this.refreshStats()
     })
