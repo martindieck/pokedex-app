a63e0fab1c780328ad2bd3b1681b9f9909f3f5d4
martindieck-syscap <krutterlax@gmail.com>
2025-04-22 12:19:54 -0600

    Changed total pokemon count in profiles view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,7 @@ class ProfilesController < ApplicationController
   def show
     @caught_pokemons = current_user.pokemons.joins(:user_pokemons).where(user_pokemons: { shiny: false }).distinct.sort_by(&:pokedex_number)
     @caught_count = @caught_pokemons.count
-    @total_pokemon = 151  # Total number of original Pokémon
+    @total_pokemon = 1025  # Total number of original Pokémon
 
     @caught_shinies = current_user.pokemons.joins(:user_pokemons).where(user_pokemons: { shiny: true }).distinct.sort_by(&:pokedex_number)
     @shiny_count = @caught_shinies.count
